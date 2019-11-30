b9974c15c48291c3f857a21c5938d69cc04e75b6
Navarro Presas Moisés Alejandro <moisalejandro@gmail.com>
2019-11-30 13:50:00 -0600

    [IMP] several movements of fields

--- __manifest__.py
+++ __manifest__.py
@@ -2,7 +2,7 @@
 
 {
     'name': 'Dogma Partner Fields',
-    'version': '13.0.1.3.0',
+    'version': '13.0.1.4.0',
     'author': 'HomebrewSoft',
     'website': 'https://github.com/HomebrewSoft/dogma_partner_fields',
     'category': 'Operations',

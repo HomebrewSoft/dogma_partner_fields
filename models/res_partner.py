3090d3771525fef622ad0a4f061248241016c1d5
Ariel Marinez <amrosasmartinez@gmail.com>
2021-09-14 01:29:45 +0200

    [ADD] new field text, default contact type, name x title - title x name

--- models/res_partner.py
+++ models/res_partner.py
@@ -4,24 +4,21 @@ from odoo import api, fields, models, _
 
 
 class Partner(models.Model):
-    _inherit = 'res.partner'
+    _inherit = "res.partner"
 
-    neighboor = fields.Char(
-    )
-    reference = fields.Char(
-    )
-    phone2 = fields.Char(
-    )
-    email2 = fields.Char(
-    )
+    neighboor = fields.Char()
+    reference = fields.Char()
+    phone2 = fields.Char()
+    email2 = fields.Char()
     category_parent_ids = fields.Many2many(
-        related='parent_id.category_id',
-        string=_('Etiquetas de la Cia.'),
+        related="parent_id.category_id",
+        string=_("Etiquetas de la Cia."),
     )
     phone_direct = fields.Char(
-        string=_('Direct'),
+        string=_("Direct"),
     )
     phone_direct_ext = fields.Char(
-        string=_('Ext.'),
+        string=_("Ext."),
         size=5,
     )
+    mark = fields.Text()
